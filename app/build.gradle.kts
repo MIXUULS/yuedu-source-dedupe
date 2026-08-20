74ba74fe8d970b92a7f9f49274ea470f6fb86561
MIXUULS <2414103761@qq.com>
2026-08-21 02:21:16 +0800

    v3.2.0: 版本号升级、README 更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mina.yuedu"
         minSdk = 24
         targetSdk = 35
-        versionCode = 304
-        versionName = "3.1.0"
+        versionCode = 305
+        versionName = "3.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
